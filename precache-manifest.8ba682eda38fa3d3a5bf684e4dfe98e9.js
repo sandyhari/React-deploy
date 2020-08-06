c7864cc22f9924b590a9e174c55d04e25864eedd
srihari boda <sandyhari333@gmail.com>
2020-08-06 11:45:44 +0530

    Updates

---
+++ precache-manifest.8ba682eda38fa3d3a5bf684e4dfe98e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecb3956fca68c501977dfd6656d17bd",
+    "url": "/React-deploy/index.html"
+  },
+  {
+    "revision": "277b42d696a52522c404",
+    "url": "/React-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f1db8604f4b6f8c7e4a",
+    "url": "/React-deploy/static/js/2.a0414d5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-deploy/static/js/2.a0414d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277b42d696a52522c404",
+    "url": "/React-deploy/static/js/main.5e580c56.chunk.js"
+  },
+  {
+    "revision": "a5fa1063b673498b1bc7",
+    "url": "/React-deploy/static/js/runtime-main.448125eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
